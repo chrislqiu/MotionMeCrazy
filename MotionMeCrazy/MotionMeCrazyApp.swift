810a2735e409c9ce320ce5f230fdc660aeb74887
Chris <chris.qiu52@gmail.com>
2025-02-06 10:35:11 -0500

    Initial Commit

---
+++ MotionMeCrazy/MotionMeCrazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionMeCrazyApp.swift
+//  MotionMeCrazy
+//
+//  Created by Chris Qiu on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MotionMeCrazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotionMeCrazyTests/MotionMeCrazyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotionMeCrazyTests.swift
+//  MotionMeCrazyTests
+//
+//  Created by Chris Qiu on 2/6/25.
+//
+
+import Testing
+@testable import MotionMeCrazy
+
+struct MotionMeCrazyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
