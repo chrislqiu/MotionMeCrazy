db696fb3af18e365bb6696c203f762cecad31e32
Ethan Donahue <donahue.ethanj@gmail.com>
2025-03-05 19:51:28 -0500

    app stata

---
+++ MotionMeCrazy/Models/AppState.swift
@@ -0,0 +1,13 @@
+//
+//  AppState.swift
+//  MotionMeCrazy
+//
+//  Created by Ethan Donahue on 3/5/25.
+//
+
+import Combine
+
+class AppState: ObservableObject {
+    @Published var offlineMode: Bool = false
+    @Published var loading: Bool = true
+}
