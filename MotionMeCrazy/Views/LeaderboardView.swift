e097464c2688980d4cedb273814e4d9a4b60b968
tlazaret <89759749+tlazaret@users.noreply.github.com>
2025-04-10 00:43:23 -0400

    changed back button

--- MotionMeCrazy/Views/LeaderboardView.swift
+++ MotionMeCrazy/Views/LeaderboardView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct LeaderboardView: View {
-    @State private var publicLeaderboardVisible = true  // track which leader board
+    @State private var publicLeaderboardVisible = true  // track which leaderboard
     @ObservedObject var userViewModel: UserViewModel
     @EnvironmentObject var appState: AppState
+    @Environment(\.presentationMode) var presentationMode  // access the presentation mode
     
     let leaderboardEntries: [(name: String, score: Int)] = [
         ("FierceOtter123", 120),
@@ -25,133 +26,142 @@ struct LeaderboardView: View {
     ]
 
     var body: some View {
-        ZStack {
-
-            Image("background")
-                .resizable()
-                .ignoresSafeArea()
-            
-            VStack {
+        NavigationView {
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .ignoresSafeArea()
                 
-                Text("Leaderboard")
-                    .font(.largeTitle)
-                    .foregroundColor(.darkBlue)
+                VStack {
+                    Text("Leaderboard")
+                        .font(.largeTitle)
+                        .foregroundColor(.darkBlue)
+                        .padding()
+
+                    HStack {
+                        Button(action: {
+                            withAnimation {
+                                publicLeaderboardVisible = true
+                            }
+                        }) {
+                            Text("Public")
+                                .font(.headline)
+                                .padding()
+                                .background(publicLeaderboardVisible ? Color.darkBlue : Color.clear)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+
+                        Button(action: {
+                            withAnimation {
+                                publicLeaderboardVisible = false
+                            }
+                        }) {
+                            Text("Friends")
+                                .font(.headline)
+                                .padding()
+                                .background(publicLeaderboardVisible ? Color.clear : Color.darkBlue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
                     .padding()
 
-              
-                HStack {
-                    Button(action: {
-                        withAnimation {
-                            publicLeaderboardVisible = true
-                        }
-                    }) {
-                        Text("Public")
-                            .font(.headline)
-                            .padding()
-                            .background(publicLeaderboardVisible ? Color.darkBlue : Color.clear)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
+                    ZStack {
+                        if publicLeaderboardVisible {
+                            VStack {
+                                Text("Public Leaderboard")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .padding()
 
-                    Button(action: {
-                        withAnimation {
-                            publicLeaderboardVisible = false
-                        }
-                    }) {
-                        Text("Friends")
-                            .font(.headline)
-                            .padding()
-                            .background(publicLeaderboardVisible ? Color.clear : Color.darkBlue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                }
-                .padding()
+                                ScrollView {
+                                    VStack(spacing: 10) {
+                                        ForEach(leaderboardEntries, id: \.name) { entry in
+                                            HStack {
+                                                //TODO: ADD REAL USER DATA HEREE
+                                                Image("pfp3") /
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(width: 40, height: 40)
+                                                    .clipShape(Circle())
+                                                    .overlay(Circle().stroke(.darkBlue, lineWidth: 3))
 
-                ZStack {
-                    if publicLeaderboardVisible {
-                        VStack {
-                            Text("Public Leaderboard")
-                                .font(.title)
-                                .foregroundColor(.white)
-                                .padding()
+                                                VStack(alignment: .leading) {
+                                                    Text(entry.name)
+                                                        .font(.headline)
+                                                        .foregroundColor(.white)
+                                                    Text("Score: \(entry.score)")
+                                                        .font(.subheadline)
+                                                        .foregroundColor(.gray)
+                                                }
 
-                            ScrollView {
-                                VStack(spacing: 10) {
-                                    ForEach(leaderboardEntries, id: \.name) { entry in
-                                        HStack {
-                                            // TODO: ADD ACTUAL PFP HERE
-                                            Image("pfp3") // Replace pfp1 with the actual image name
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 40, height: 40)
-                                                .clipShape(Circle())
-                                                .overlay(Circle().stroke(.darkBlue, lineWidth: 3))
-
-                                            VStack(alignment: .leading) {
-                                                Text(entry.name)
-                                                    .font(.headline)
-                                                    .foregroundColor(.white)
-                                                Text("Score: \(entry.score)")
-                                                    .font(.subheadline)
-                                                    .foregroundColor(.gray)
+                                                Spacer()
                                             }
-
-                                            Spacer()
+                                            .padding()
+                                            .background(Color("DarkBlue").opacity(0.8))
+                                            .cornerRadius(10)
                                         }
-                                        .padding()
-                                        .background(Color("DarkBlue").opacity(0.8))
-                                        .cornerRadius(10)
                                     }
+                                    .padding()
                                 }
-                                .padding()
                             }
-                        }
-                        .transition(.move(edge: .leading))
-                    } else {
-                        VStack {
-                            Text("Friends Leaderboard")
-                                .font(.title)
-                                .foregroundColor(.white)
-                                .padding()
+                            .transition(.move(edge: .leading))
+                        } else {
+                            VStack {
+                                Text("Friends Leaderboard")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .padding()
 
-                            ScrollView {
-                                VStack(spacing: 10) {
-                                    ForEach(friendsLeaderboardEntries, id: \.name) { entry in
-                                        HStack {
-                                            // TODO: ADD ACTUAL PFP HERE
-                                            Image("pfp1")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 40, height: 40)
-                                                .clipShape(Circle())
-                                                .overlay(Circle().stroke(.darkBlue, lineWidth: 3))
+                                ScrollView {
+                                    VStack(spacing: 10) {
+                                        ForEach(friendsLeaderboardEntries, id: \.name) { entry in
+                                            HStack {
+                                                //TODO: ADD REAL USER DATA HEREE
+                                                Image("pfp1")
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(width: 40, height: 40)
+                                                    .clipShape(Circle())
+                                                    .overlay(Circle().stroke(.darkBlue, lineWidth: 3))
 
-                                            VStack(alignment: .leading) {
-                                                Text(entry.name)
-                                                    .font(.headline)
-                                                    .foregroundColor(.white)
-                                                Text("Score: \(entry.score)")
-                                                    .font(.subheadline)
-                                                    .foregroundColor(.gray)
+                                                VStack(alignment: .leading) {
+                                                    Text(entry.name)
+                                                        .font(.headline)
+                                                        .foregroundColor(.white)
+                                                    Text("Score: \(entry.score)")
+                                                        .font(.subheadline)
+                                                        .foregroundColor(.gray)
+                                                }
+
+                                                Spacer()
                                             }
-
-                                            Spacer()
+                                            .padding()
+                                            .background(Color("DarkBlue").opacity(0.8))
+                                            .cornerRadius(10)
                                         }
-                                        .padding()
-                                        .background(Color("DarkBlue").opacity(0.8))
-                                        .cornerRadius(10)
                                     }
+                                    .padding()
                                 }
-                                .padding()
                             }
+                            .transition(.move(edge: .trailing))
                         }
-                        .transition(.move(edge: .trailing))
                     }
+                    .animation(.easeInOut, value: publicLeaderboardVisible)
                 }
-                .animation(.easeInOut, value: publicLeaderboardVisible) 
             }
+            .background(Color.black.ignoresSafeArea())
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action: {
+                // bye
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "arrow.left.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.darkBlue)
+                    .padding(.bottom, 40)
+            })
         }
-        .background(Color.black.ignoresSafeArea())
     }
 }
